f09a4ed42534c8ea0ea17c2f4ebdd758be9b4e59
andrew-hayworth22 <mhaywort@kent.edu>
2024-08-12 13:41:04 -0400

    Added easy config in main.py for maze rows and columns

--- src/main.py
+++ src/main.py
@@ -2,10 +2,15 @@ from window import Window
 from geometry import Point, Line
 from maze import Cell, Maze
 
-def main():
-    window = Window(520, 520)
 
-    maze = Maze(window=window)
+def main():
+    # Update these values to configure your maze
+    rows = 10
+    columns = 10
+
+    window = Window((rows * 50)+20, (columns * 50)+20)
+
+    maze = Maze(window=window, num_rows=rows, num_cols=columns)
     maze.solve()
 
     window.wait_for_close()
